9fc320cb3bb65d04c267a5fdc0a3993a50c4ae2a
MDixey17 <matthew_dixey@yahoo.com>
2024-02-04 09:48:47 -0600

    working postgame, added HC assets

--- src/components/PlayerBoostBar/PlayerBoostBar.style.ts
+++ src/components/PlayerBoostBar/PlayerBoostBar.style.ts
@@ -26,10 +26,11 @@ export const BoostBarDefault = styled.div<{ index: number }>`
 export const BoostBarSliding = styled.div<{
   width: number;
   index: number;
+  endColor: string;
 }>`
   height: ${PLAYER_BOOST_BAR.boostBarHeight}px;
   width: ${(props) => props.width}px;
-  background: linear-gradient(to right, yellow, red);
+  background: linear-gradient(to right, yellow, ${(props) => props.endColor});
   position: absolute;
   top: ${(props) => props.index * 52.8 + 37}px;
   z-index: 9999;
--- src/components/PlayerBoostBar/PlayerBoostBar.tsx
+++ src/components/PlayerBoostBar/PlayerBoostBar.tsx
@@ -25,6 +25,32 @@ export const PlayerBoostBar = ({
   primaryColor,
   secondaryColor,
 }: PlayerBoostBarProps) => {
+  const computeEndColor = (): string => {
+    const yellowHex: string = "FFFF00";
+    const redHex: string = "FF0000";
+    const ratio = boostAmount / 100;
+
+    const toHex = (n: number) => {
+      const nString = n.toString(16);
+      return nString.length === 1 ? "0" + nString : nString;
+    };
+
+    const r = Math.ceil(
+      parseInt(redHex.substring(0, 2), 16) * ratio +
+        parseInt(yellowHex.substring(0, 2), 16) * (1 - ratio)
+    );
+    const g = Math.ceil(
+      parseInt(redHex.substring(2, 4), 16) * ratio +
+        parseInt(yellowHex.substring(2, 4), 16) * (1 - ratio)
+    );
+    const b = Math.ceil(
+      parseInt(redHex.substring(4, 6), 16) * ratio +
+        parseInt(yellowHex.substring(4, 6), 16) * (1 - ratio)
+    );
+
+    return "#" + toHex(r) + toHex(g) + toHex(b);
+  };
+
   return (
     <BoostBarWrapper
       primary={primaryColor}
@@ -42,6 +68,7 @@ export const PlayerBoostBar = ({
           PLAYER_BOOST_BAR.width
         )}
         index={index}
+        endColor={computeEndColor()}
       />
       <BoostBarDefault index={index} />
     </BoostBarWrapper>
